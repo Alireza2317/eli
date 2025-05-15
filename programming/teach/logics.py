a1f063f6c59e83d34d69cf7cbe6cba5a986b08f1
Alireza <alireza.alavi237@gmail.com>
2025-05-15 17:33:46 +0330

    taugh logics like and or not

--- programming/teach/logics.py
+++ programming/teach/logics.py
@@ -1,4 +1,4 @@
-x = -12
+x = 12
 y = -7
 
 print((x > 0) or (y > 0))
@@ -6,3 +6,20 @@ print((x > 0) and (y > 0))
 
 # and: returns the first if falsy  else the second
 # or:  returns the first if truthy else the second
+
+
+name = ''
+print('hello ' + (name or 'user23'))
+
+# falsy
+falsies = [
+	False, '', [], tuple(), list(), set(), 0, int(), float(), None, dict()
+]
+
+#truthy
+#anything other than those above
+
+
+# bool
+for f in falsies:
+	print(bool(f))
